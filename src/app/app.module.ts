5c0f1c6f42eb9204425c0eda64364f9e44f82c68
Elisaul.Morillo <elisaul.morillo@nagico.com>
2019-10-30 16:44:29 -0400

    Add html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule }from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    ProfileComponent
+    ProfileComponent,
+ 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAngularModule
+    AmplifyAngularModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
   providers: [AmplifyService],
   bootstrap: [AppComponent]
