069dd03ea669864301cb7f26556f24f43d15c8fa
Elisaul.Morillo <elisaul.morillo@nagico.com>
2019-10-31 15:55:25 -0400

    Set config to create user

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { APIService } from '../API.service';
+import { Auth } from 'aws-amplify';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  userId: string;
+  userName:string;
+  user = new User('','',''); 
+  constructor(private api:APIService) { }
 
   ngOnInit() {
+  Auth.currentAuthenticatedUser()
+    .then(user => {
+      return Auth.changePassword(user, 'oldPassword', 'newPassword');
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+    
   }
+    async updateProfile() {
+      const userCreate = {
+        id:this.userId,
+        username: this.user.username,
+        firstName:this.user.firstName,
+        lastname:"",
+        bio:""
+      }
+       await this.api.CreateUser(userCreate);
+     
+    }
 
 }
